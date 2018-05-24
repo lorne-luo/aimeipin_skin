c05a98f3ac7947328ada41e33f3a1e6f686791a9
Lorne <dev@luotao.net>
2018-05-24 21:16:34 +1000

    update survey models, make fields optional

--- apps/survey/models.py
+++ apps/survey/models.py
@@ -137,14 +137,22 @@ class Answer(models.Model):
                                             max_length=255, help_text='提示：如没有，请留空')  # No. 64
 
     # 非选项问题 产品选择题 65-72
-    cosmetic_products1 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products1', blank=True, verbose_name=u'65. 目前正在使用的卸妆类的护肤品名')
-    cosmetic_products2 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products2', blank=True, verbose_name=u'66、目前正在使用的洁面乳/洁面霜/洁面油类的护肤品名称')
-    cosmetic_products3 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products3', blank=True, verbose_name=u'67、目前正在所使用化妆水类护肤品名称')
-    cosmetic_products4 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products4', blank=True, verbose_name=u'68、目前正在所使用乳液／面霜类护肤品名称')
-    cosmetic_products5 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products5', blank=True, verbose_name=u'69、目前正在使用的精华类的护肤品名称')
-    cosmetic_products6 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products6', blank=True, verbose_name=u'70、目前正在使用的去角质类的护肤品名称')
-    cosmetic_products7 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products7', blank=True, verbose_name=u'71、目前正在使用的的面膜类的护肤品名称')
-    cosmetic_products8 = models.ManyToManyField('AnswerProduct',related_name='cosmetic_products8', blank=True, verbose_name=u'72、目前正在使用的防晒类的护肤品名称')
+    cosmetic_products1 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products1', null=True,
+                                                blank=True, verbose_name=u'65. 目前正在使用的卸妆类的护肤品名')
+    cosmetic_products2 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products2', null=True,
+                                                blank=True, verbose_name=u'66、目前正在使用的洁面乳/洁面霜/洁面油类的护肤品名称')
+    cosmetic_products3 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products3', null=True,
+                                                blank=True, verbose_name=u'67、目前正在所使用化妆水类护肤品名称')
+    cosmetic_products4 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products4', null=True,
+                                                blank=True, verbose_name=u'68、目前正在所使用乳液／面霜类护肤品名称')
+    cosmetic_products5 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products5', null=True,
+                                                blank=True, verbose_name=u'69、目前正在使用的精华类的护肤品名称')
+    cosmetic_products6 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products6', null=True,
+                                                blank=True, verbose_name=u'70、目前正在使用的去角质类的护肤品名称')
+    cosmetic_products7 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products7', null=True,
+                                                blank=True, verbose_name=u'71、目前正在使用的的面膜类的护肤品名称')
+    cosmetic_products8 = models.ManyToManyField('AnswerProduct', related_name='cosmetic_products8', null=True,
+                                                blank=True, verbose_name=u'72、目前正在使用的防晒类的护肤品名称')
 
     other_question1 = models.TextField('73、您是否每天（一年四季，不管晴天、阴天、雨天，室内室外）涂抹足量（面部一元硬币大小）专门的防晒产品（不包括隔离霜，底妆）？', blank=True,
                                        max_length=255)  # No. 73
@@ -154,7 +162,7 @@ class Answer(models.Model):
     status = models.CharField(u'状态', choices=SURVEY_STATUS_CHOICES, blank=False, max_length=32, default='CREATED')
     is_changeable = models.BooleanField(u'是否可修改', default=True, blank=False)
     created_at = models.DateTimeField(u"创建时间", auto_now_add=True)
-    remark = models.TextField(u'备注', blank=False, max_length=255)
+    remark = models.TextField(u'备注', blank=True, max_length=255)
 
     objects = AnswerManager()
 
@@ -210,3 +218,8 @@ class AnswerProduct(models.Model):
             return str(self.product)
         else:
             return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.product:
+            self.name = str(self.product)
+        super(AnswerProduct, self).save(*args, **kwargs)

2ffe17cacf91cb82185ddc928bd56aa3672c1459
Lorne <dev@luotao.net>
2018-06-02 21:06:34 +1000

    changeable button on answer list

--- apps/survey/static/js/survey/answer_list.js
+++ apps/survey/static/js/survey/answer_list.js
@@ -1,4 +1,48 @@
-var answerListPageVue = new CommonListPageVue({
+var AnswerListPageVue = CommonListPageVue.extend({
+    methods: {
+        update_changeable: function (id, event) {
+            var currentValue = $(event.target).hasClass('active');
+            var target = $(event.target);
+            var text = !currentValue ? '可以' : '不可';
+            var url = Urls[this.detail_api_tag](id);
+
+            swal({
+                title: "更改可否修改",
+                text: "确认将问卷变更为\"" + text + "\"修改?",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                closeOnConfirm: true,
+                showLoaderOnConfirm: false,
+                animation: false
+            }, function () {
+                $.AdminLTE.apiPost(
+                    url,
+                    $.param({'is_changeable': !currentValue}),
+                    function (resp) {
+                        if (currentValue) {
+                            target.removeClass('active');
+                            target.removeClass('btn-success');
+                            target.addClass('btn-danger');
+                            target.text('否');
+                        } else {
+                            target.addClass('active');
+                            target.removeClass('btn-danger');
+                            target.addClass('btn-success');
+                            target.text('是');
+                        }
+                    }
+                );
+            });
+
+
+        }
+    }
+});
+
+var answerListPageVue = new AnswerListPageVue({
         data: {
             // API
             list_api_tag: 'api:answer-list',

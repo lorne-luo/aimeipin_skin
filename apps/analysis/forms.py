e05cd1e825f262be19df550e75a2cb3e1b63eed8
Lorne <dev@luotao.net>
2018-06-05 19:47:26 +1000

    limit skintype options for word

--- apps/analysis/forms.py
+++ apps/analysis/forms.py
@@ -8,6 +8,13 @@ class WordAddForm(forms.ModelForm):
         model = Word
         exclude = ['pinyin']
 
+    def __init__(self, *args, **kwargs):
+        super(WordAddForm, self).__init__(*args, **kwargs)
+        self.fields['oily_type'].queryset = SkinType.objects.filter(dimension='油性or干性')
+        self.fields['sensitive_type'].queryset = SkinType.objects.filter(dimension='敏感or耐受')
+        self.fields['pigment_type'].queryset = SkinType.objects.filter(dimension='色素or非色素')
+        self.fields['loose_type'].queryset = SkinType.objects.filter(dimension='易皱纹or紧致')
+
 
 class WordUpdateForm(WordAddForm):
     class Meta:
@@ -21,19 +28,19 @@ class WordDetailForm(WordAddForm):
         exclude = ['pinyin']
 
 
-
 class SkinTypeAddForm(forms.ModelForm):
     class Meta:
         model = SkinType
-        fields='__all__'
+        fields = '__all__'
+
 
 class SkinTypeUpdateForm(forms.ModelForm):
     class Meta:
         model = SkinType
-        fields='__all__'
+        fields = '__all__'
 
 
 class SkinTypeDetailForm(forms.ModelForm):
     class Meta:
         model = SkinType
-        fields='__all__'
+        fields = '__all__'

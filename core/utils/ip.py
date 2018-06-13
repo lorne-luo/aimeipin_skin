ebe692b6d9b98ac8f73858950cf7378492337a38
Lorne <dev@luotao.net>
2018-06-14 09:11:22 +1000

    update ip to address

--- core/utils/ip.py
+++ core/utils/ip.py
@@ -3,17 +3,23 @@ import requests
 
 def get_location(ip):
     base_url = 'http://ip.taobao.com//service/getIpInfo.php?ip='
-    url = '%s%s' % (base_url, ip)
-    r = requests.get(url)
+    try:
+        url = '%s%s' % (base_url, ip)
+        r = requests.get(url)
 
-    if r.status_code == 200:
-        json = r.json()
-        code = json['code']
-        if code == 0:
-            data = json['data']
-            if data['country'] == '中国':
-                return '%s %s' % (data['region'], data['city'])
-            else:
-                return '%s %s %s' % (data['country'], data['region'], data['city'])
+        if r.status_code == 200:
+            json = r.json()
+            code = json['code']
+            if code == 0:
+                data = json['data']
+                if data['country'] == '中国':
+                    if data['region'] == data['city']:
+                        return data['city']
+                    else:
+                        return '%s %s' % (data['region'], data['city'])
+                else:
+                    return '%s %s %s' % (data['country'], data['region'], data['city'])
+    except:
+        pass
 
     return None

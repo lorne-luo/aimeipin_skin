913e4bf8e23234ddad370efdd4ea917bb2a5835e
Lorne <dev@luotao.net>
2018-06-14 14:30:59 +1000

    Revert "add celery configuration"
    This reverts commit c761eb7ad6fb6c288673c839a96f0ec438c23c08.

--- config/celery.py
+++ config/celery.py
@@ -1,16 +1,14 @@
+from __future__ import absolute_import
+
 import os
 from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+app = Celery('settings')
 
-app = Celery('aimeipin_skin')
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
-
-# Load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-app.conf.task_default_queue = 'aimeipin_skin'
